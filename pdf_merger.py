356f0121192e9a0e217d45ff675b7a05e510c06b
MuadDAtr <int.stron@protonmail.com>
2022-11-10 00:05:15 +0100

    adding features

--- pdf_merger.py
+++ pdf_merger.py
@@ -2,7 +2,8 @@ import PyPDF2
 import sys
 import os
 
-merger = PyPDF2.PdfFileMerger()
+merging = PyPDF2.PdfFileMerger()
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
-        
+        merging.append(file)
+    merging.write("MergedDocument.pdf")
